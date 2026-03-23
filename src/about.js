983d99d057a18d6a15b290187ca93dbd7dfbbf61
synergeeee <kimapour@gmail.com>
2026-03-22 21:40:30 -0700

    Refactored about.js
    added two helper functions to reduce code repetition

--- src/about.js
+++ src/about.js
@@ -1,47 +1,42 @@
 export function loadAbout() {
     const content = document.getElementById('content');
-    const about = document.createElement('div')
-    about.classList.add('about');
+    const about = createElement('div', 'about');
     content.append(about);
 
-    const menuSection1 = document.createElement('div');
-    menuSection1.classList.add('about-section');
+    const aboutSectionOne = createSection(
+        'Our Story',
+        "Caravan Kitchen was born from a love for Uzbek culinary traditions. Inspired by the rich flavors of the Silk Road, we bring authentic recipes to life using fresh, local ingredients. Since our opening, we’ve been serving classic dishes like plov, somsa, and shashlik, crafted with care by our skilled chefs, all in a warm, inviting atmosphere that celebrates the spirit of Uzbek hospitality."
+    );
 
-    const section1Header = document.createElement('h2');
-    section1Header.classList.add('about-section-header')
-    section1Header.textContent = "Our Story"
+    const aboutSectionTwo = createSection(
+        'Location',
+        `📍 5 Usta Olim St, 100109 <br>  Tashkent, Uzbekistan`
+    );
 
-    const section1Text = document.createElement('p');
-    section1Text.classList.add('about-section-text');
-    section1Text.textContent = "Caravan Kitchen was born from a love for Uzbek culinary traditions. Inspired by the rich flavors of the Silk Road, we bring authentic recipes to life using fresh, local ingredients. Since our opening, we’ve been serving classic dishes like plov, somsa, and shashlik, crafted with care by our skilled chefs, all in a warm, inviting atmosphere that celebrates the spirit of Uzbek hospitality."
-    menuSection1.append(section1Header, section1Text);
+    const menuSectionThree = createSection(
+        "Contact",
+        `📞 (71) 006 77 67 <br>📧  caravan@assist.com`,
+    );
 
+    about.append(aboutSectionOne, aboutSectionTwo, menuSectionThree);
+}
 
-    const menuSection2 = document.createElement('div');
-    menuSection2.classList.add('about-section');
+function createElement(elementType, elementclass) {
+    const element = document.createElement(elementType);
+    element.classList.add(elementclass);
 
-    const section2Header = document.createElement('h2');
-    section2Header.classList.add('about-section-header')
-    section2Header.textContent = "Location"
+    return element;
+}
 
-    const section2Text = document.createElement('p');
-    section2Text.classList.add('about-section-text');
-    section2Text.innerHTML = `📍 5 Usta Olim St, 100109 <br>  Tashkent, Uzbekistan`;
+function createSection(heading, innerHtml) {
+    const sectionDiv = createElement('div', 'about-section');
+    const sectionHeading = createElement('h2', 'about-section-header');
+    const sectionText = createElement('p', 'about-section-text');
 
-    menuSection2.append(section2Header, section2Text);
+    sectionHeading.textContent = heading;
+    sectionText.innerHTML = innerHtml;
 
-
-    const menuSection3 = document.createElement('div');
-    menuSection3.classList.add('about-section-last');
-
-    const section3Header = document.createElement('h2');
-    section3Header.classList.add('about-section-header')
-    section3Header.textContent = "Contact"
-
-    const section3Text = document.createElement('p');
-    section3Text.classList.add('about-section-text');
-    section3Text.innerHTML = `📞 (71) 006 77 67 <br>📧  caravan@assist.com`;
-    menuSection3.append(section3Header, section3Text);
-
-    about.append(menuSection1, menuSection2, menuSection3);
+    sectionDiv.appendChild(sectionHeading);
+    sectionDiv.appendChild(sectionText);
+    return sectionDiv;
 }
