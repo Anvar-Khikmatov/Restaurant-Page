2862bb7b99bce4ea2ed450774bcead19c14eee41
synergeeee <kimapour@gmail.com>
2026-03-24 16:19:30 -0700

    fix duplicate menu div, other small changes
    also made the double quotes be single quotes for consistency

--- src/about.js
+++ src/about.js
@@ -1,7 +1,10 @@
+import { createElement, propertyType } from './utils.js';
+
 export function loadAbout() {
     const content = document.getElementById('content');
-    const about = createElement('div', 'about');
-    content.append(about);
+    const aboutDiv = createElement('div', propertyType.CLASS, 'about');
+
+    content.append(aboutDiv);
 
     const aboutSectionOne = createSection(
         'Our Story',
@@ -10,33 +13,26 @@ export function loadAbout() {
 
     const aboutSectionTwo = createSection(
         'Location',
-        `📍 5 Usta Olim St, 100109 <br>  Tashkent, Uzbekistan`
+        "📍 5 Usta Olim St, 100109 <br>  Tashkent, Uzbekistan"
     );
 
     const menuSectionThree = createSection(
         "Contact",
-        `📞 (71) 006 77 67 <br>📧  caravan@assist.com`,
+        "📞 (71) 006 77 67 <br>📧  caravan@assist.com",
     );
 
-    about.append(aboutSectionOne, aboutSectionTwo, menuSectionThree);
+    aboutDiv.append(aboutSectionOne, aboutSectionTwo, menuSectionThree);
 }
-
-function createElement(elementType, elementclass) {
-    const element = document.createElement(elementType);
-    element.classList.add(elementclass);
-
-    return element;
-}
-
 function createSection(heading, innerHtml) {
-    const sectionDiv = createElement('div', 'about-section');
-    const sectionHeading = createElement('h2', 'about-section-header');
-    const sectionText = createElement('p', 'about-section-text');
+    const sectionDiv = createElement('div', propertyType.CLASS, 'about-section');
+    const sectionHeading = createElement('h2', propertyType.CLASS, 'about-section-header');
+    const sectionText = createElement('p', propertyType.CLASS, 'about-section-text');
 
     sectionHeading.textContent = heading;
     sectionText.innerHTML = innerHtml;
 
     sectionDiv.appendChild(sectionHeading);
     sectionDiv.appendChild(sectionText);
+
     return sectionDiv;
 }
